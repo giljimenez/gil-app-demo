13fc55e26b620ad77ec46eaaccdb426dd1ba1ac1
Gil Jimenez <giljimenez@icloud.com>
2020-03-16 20:55:40 -0500

    Initial Commit

---
+++ gil-demo-app/AccountViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class AccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+
+}
+
---
+++ gil-demo-app/ShopViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class ShopViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+
+}
+
